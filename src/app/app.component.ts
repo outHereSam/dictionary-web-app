dcaf5c771332a9bc777f53de64ccde9c35e8af71
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-07-17 11:18:36 +0000

    Styles search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
